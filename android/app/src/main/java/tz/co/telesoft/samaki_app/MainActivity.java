24d9f8fbdc0e3e47c0c69204723fec5ea81038b5
lusajo143 <techdealers143@gmail.com>
2022-02-18 02:26:45 +0300

    initial commit

---
+++ android/app/src/main/java/tz/co/telesoft/samaki_app/MainActivity.java
@@ -0,0 +1,6 @@
+package tz.co.telesoft.samaki_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Before/Splash.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            child: const Image(image: AssetImage("assets/bg.png"),
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,),
+            color: Colors.white,
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                const Image(image: AssetImage("assets/logo.png"),
+                width: 50,),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: const Text("Samaki App",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ))
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
